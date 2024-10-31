d2070c1addf266ca884eabb607319aff35f68ea2
jesse finneman <jfskibum123@yahoo.com>
2024-10-31 12:53:07 -0600

    update unit test

---
+++ TicTacToe/qtests/test_systemsettings.h
@@ -0,0 +1,15 @@
+#ifndef TEST_SYSTEMSETTINGS_H
+#define TEST_SYSTEMSETTINGS_H
+
+#include <QObject>
+
+class TestSystemSettings : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TestSystemSettings(QObject *parent = nullptr);
+
+signals:
+};
+
+#endif // TEST_SYSTEMSETTINGS_H
