fb0e97f02826ef149e017e5ac1da3cb1d11f76cb
jesse finneman <jfskibum123@yahoo.com>
2024-10-31 00:14:22 -0600

    database manager unit tests

---
+++ TicTacToe/qtests/test_databasemanager.cpp
@@ -0,0 +1,51 @@
+#include "test_databasemanager.h"
+#include "../controllers/databasemanager.h"
+
+void TestDatabaseManager::initTestCase()
+{
+    // Code to set up the test environment before any test functions are executed
+}
+
+void TestDatabaseManager::cleanupTestCase()
+{
+    // Code to clean up the test environment after all test functions have been executed
+}
+
+void TestDatabaseManager::init()
+{
+    // Code to set up before each test function
+}
+
+void TestDatabaseManager::cleanup()
+{
+    // Code to clean up after each test function
+}
+
+// Test cases for each public method in DatabaseManager
+void TestDatabaseManager::testInitializeDatabase()
+{
+    DatabaseManager dbManager;
+    QVERIFY(dbManager.initializeDatabase() == true);
+}
+
+// Similarly, implement other test cases
+void TestDatabaseManager::testCloseDatabase()
+{
+    DatabaseManager dbManager;
+    dbManager.initializeDatabase();
+    dbManager.closeDatabase();
+    // Add assertions to verify the state after closing the database
+}
+
+void TestDatabaseManager::testGetPlayerIdByName()
+{
+    DatabaseManager dbManager;
+    dbManager.initializeDatabase();
+    int playerId = dbManager.getPlayerIdByName("PlayerName");
+    QVERIFY(playerId != -1); // Assuming -1 indicates an invalid ID
+}
+
+// Implement other test cases similarly...
+
+QTEST_MAIN(TestDatabaseManager)
+#include "test_databasemanager.moc"
